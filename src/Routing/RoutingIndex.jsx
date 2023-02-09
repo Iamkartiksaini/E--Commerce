f77b6c121f1f7232b8d2f0ade9e340ec15c1423a
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2023-02-09 13:23:42 +0530

    Create RoutingIndex.jsx

---
+++ src/Routing/RoutingIndex.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Footer from "../Component/Footer";
+import Shipping from "../Component/Shipping";
+import HomePage from "../Pages/HomePage";
+import Offers from "../Pages/Offers";
+import "./ex.scss";
+function RoutingIndex() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Shipping />
+      <Offers />
+      <Footer />
+    </>
+  );
+}
+
+export default RoutingIndex;
